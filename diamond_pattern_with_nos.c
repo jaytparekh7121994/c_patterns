85a3556789c519f8c0eed2304cc7e1ef301d273e
jaytparekh7121994 <53175164+jaytparekh7121994@users.noreply.github.com>
2019-07-22 17:27:27 +0530

    Update diamond_pattern_with_nos.c

--- diamond_pattern_with_nos.c
+++ diamond_pattern_with_nos.c
@@ -1,21 +1,37 @@
+/*This C program is to print the diamond pattern with numbers. To understand the logic please go to read me.
+Output Pattern: for n=4
+     1
+    121
+   12321
+  1234321
+   12321
+    121
+     1
+*/
+
 #include<stdio.h>
 
 int main(){
 	int i,j,k,n,l;
 	printf("Enter the no. to see the mirror pattern: ");
 	scanf("%d",&n);
+	//Upper half of the pattern ie from 1 to 1234321 
 	for(i=1;i<=n;i++){
 		for(j=1;j<=n-i;j++){
 			printf(" ");
 		}
+		
 		for(k=1;k<=i;k++){
-			printf("%d",k);
+			printf("%d",k);   
 		}
+		
 		for(l=i-1;l>=1;l--){
 			printf("%d",l);
 		}
 		printf("\n");
 	}
+	//Lower half of the patter ie from 12321 to 1
+	
 	for(i=1;i<=n;i++){
 		for(j=1;j<=i;j++){
 			printf(" ");
@@ -27,6 +43,5 @@ int main(){
 			printf("%d",l);
 		}
 		printf("\n");
-	}
-		
+	}	
 }
